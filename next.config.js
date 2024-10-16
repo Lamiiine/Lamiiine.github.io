80bd4247a0a3efc0291ec11b51ea0d4cdf59ba7a
Lamiiine <mlamine.abdellaoui@aucegypt.edu>
2024-10-16 11:05:53 +0100

    update

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // This enables static export
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "**" }],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
+  basePath: '/Lamiiine.github.io', // Replace with your repository name if using GitHub Pages
+  assetPrefix: '/Lamiiine.github.io', // Replace with your repository name
 };
 
 module.exports = withContentlayer(nextConfig);
